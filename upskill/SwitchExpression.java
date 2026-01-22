a6f30ee9ce50d71b97837ce54858e670a600ad7e
Chrisen Galang <chrisen_john_abraham_galang@manulife.com>
2026-01-22 12:10:14 -0500

    Java 14 yield in switch

--- upskill/SwitchExpression.java
+++ upskill/SwitchExpression.java
@@ -1,10 +1,13 @@
 public class SwitchExpression {
     public static void main(String[] args) {
-        String day = "Monday";
+        String day = "Sat";
 
         boolean isWeekday = switch (day) {
             case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> true;
-            default -> false;
+            default -> {
+                System.out.println("Either a weekend or input is invalid.");
+                yield false;
+            }
         };
 
         System.out.println(isWeekday ? "Today is a weekday" : "Today is a weekend");
