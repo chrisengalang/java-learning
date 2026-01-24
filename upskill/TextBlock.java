3b8caf2c246aea66ab5c15db6a05bbbfbff1491e
Chrisen Galang <chrisen_john_abraham_galang@manulife.com>
2026-01-24 17:48:26 -0500

    Add New Text Block

---
+++ upskill/TextBlock.java
@@ -0,0 +1,13 @@
+public class TextBlock {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String newTextBlock = """
+                Hello there
+                My name is Chrisen John Abraham Galang
+                I am 34 years old.
+                """;
+
+        System.out.println(newTextBlock);
+    }
+}
